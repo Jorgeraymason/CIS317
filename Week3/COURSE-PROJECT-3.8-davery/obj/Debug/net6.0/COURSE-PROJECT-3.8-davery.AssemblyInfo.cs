26608054298de916d2833653dd5ff5cdb13cd90d
Jorgeraymason <billchakor@gmail.com>
2024-10-23 12:33:07 -0400

    First commet

--- Week3/COURSE-PROJECT-3.8-davery/obj/Debug/net6.0/COURSE-PROJECT-3.8-davery.AssemblyInfo.cs
+++ Week3/COURSE-PROJECT-3.8-davery/obj/Debug/net6.0/COURSE-PROJECT-3.8-davery.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("COURSE-PROJECT-3.8-davery")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20937d9d341cb6ec3880a4959e16b1119933f3bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3360b534d9f03416c3cd9ba43fe119517425fdc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("COURSE-PROJECT-3.8-davery")]
 [assembly: System.Reflection.AssemblyTitleAttribute("COURSE-PROJECT-3.8-davery")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Week4/GP-4.3-davery/Program.cs
+++ Week4/GP-4.3-davery/Program.cs
@@ -1,2 +1,77 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*******************************************************************
+* Name: Devin Avery
+* Date: 10/23/24
+* Assignment: SDC320 Week 4 GP – Binary Files
+*
+* Main application class.
+*/
+using System.IO;
+public class BinaryFileDemo
+{
+    private const string FILE_NAME = "YourName.dat";
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("\nYour Name, Week 4 Text Files GP\n");
+        Console.WriteLine(WriteToFile());
+        Console.WriteLine(ReadFromFile());
+    }
+    // Write to a file - on completion, return a message indicating
+    // success or an error message if an error occurs
+    public static string WriteToFile()
+    {
+        try
+        {
+            using (BinaryWriter bWriter =
+            new BinaryWriter(File.Open(FILE_NAME, FileMode.Create)))
+            {
+                Console.WriteLine("Starting to write to file");
+                //Write a string
+                bWriter.Write("String written in binary.");
+                //Write an integer
+                bWriter.Write(5);
+                //Write a double
+                bWriter.Write(9.95F);
+                //Write a boolean
+                bWriter.Write(true);
+                Console.WriteLine("Finished writing to file\n");
+                return "Write completed successfully\n";
+            }
+        }
+        catch (IOException e)
+        {
+            return "An error occurred: " + e.Message;
+        }
+    }
+    // Read from a file - if successful, return a message indicating
+    // the file contents were printed; if an error occurs, return a
+    // message stating an error occurred
+    public static string ReadFromFile()
+    {
+        try
+        {
+            using (BinaryReader bReader =
+            new BinaryReader(File.Open(FILE_NAME, FileMode.Open)))
+            {
+                Console.WriteLine("Starting to read from file");
+                //Read a string
+                Console.Write("String value from file: ");
+                Console.WriteLine(bReader.ReadString());
+                //Read an integer
+                Console.Write("Integer value from file: ");
+                Console.WriteLine(bReader.ReadInt32());
+                //Read a double
+                Console.Write("Double value from file: ");
+                Console.WriteLine(bReader.ReadSingle());
+                //Read a boolean
+                Console.Write("Boolean value from file: ");
+                Console.WriteLine(bReader.ReadBoolean());
+                Console.WriteLine("Finished reading from file\n");
+                return "Reading completed successfully";
+            }
+        }
+        catch (IOException e)
+        {
+            return "An error occurred: " + e.Message;
+        }
+    }
+}
